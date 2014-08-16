0d4665e1d8196bfc60ebb312dd128ac6035a4b2d
Phil Jeffes <philjeffes@Phils-MacBook-Pro.local>
2014-08-16 17:39:07 +0100

    random data returned for two vars

--- pyramid/sparks/sparks/views.py
+++ pyramid/sparks/sparks/views.py
@@ -1,7 +1,7 @@
 """ Cornice services.
 """
 from cornice import Service
-
+import random
 
 hello = Service(name='hello', path='/', description="Simplest app")
 
@@ -9,4 +9,8 @@ hello = Service(name='hello', path='/', description="Simplest app")
 @hello.get()
 def get_info(request):
     """Returns Hello in JSON."""
-    return {'Hello': 'World'}
+    #height will be between 50cm and 200cm
+    #distance is distance from sensor (100cm to 50cm)
+    height = random.randint(1, 100)
+    distance = random.random(1, 100)
+    return {'data_height': height, 'data_distance': distance}
